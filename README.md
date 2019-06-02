fcc21c8143575512a83052b57e99080b7d920929
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2019-06-02 01:15:43 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 # zip-info-pwa
 
+This is a simple PWA example built with Ionic and Vue.
+
+[Live Demo](http://enes-zip-info.surge.sh/#/) - PWA is not registered.
+
+Inspired by a Traversy Media tutorial.
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Run your tests
+
 ```
 yarn run test
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
--- src/router.js
+++ src/router.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import { IonicVueRouter } from '@ionic/vue'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(IonicVueRouter)
 
-export default new Router({
+export default new IonicVueRouter({
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
